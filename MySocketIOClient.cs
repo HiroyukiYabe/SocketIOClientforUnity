e21525d6839608743e1159539465db34c7ce9525
Hiroyuki Yabe <yabe@cyber.t.u-tokyo.ac.jp>
2015-01-08 13:38:52 +0900

    initial commit

---
+++ MySocketIOClient.cs
@@ -0,0 +1,131 @@
+﻿//SocketIOClientを利用して,Socket.IOで立てたNode.jsサーバと通信するためのクラス
+//外部でこのクラスを生成し,イベントハンドラ等を設定する
+
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SocketIOClient.Messages;
+
+using DICT = System.Collections.Generic.Dictionary<string,object>;
+
+namespace MySocketIO{
+public class MySocketIOClient {
+
+	SocketIOClient.Client socket;
+	System.Guid guid;
+	string _uuid;
+	static int playerID;//Node Serverから返される一意の番号. 現在の実装では切断して再接続すると番号が変わる
+	public int myID {get {return playerID;}}
+
+	//独自に定義したメッセージの容れ物
+	public class MyMessage{
+		public int sender {get; private set;}
+		public string eventName {get; private set;}
+		public DICT message {get; private set;}
+		public MyMessage(int _sender, string _event, DICT _message){
+			sender = _sender;
+			eventName = _event;
+			message = _message;
+		}
+		public bool isMine
+		{
+			get{return sender == playerID;}
+		}
+	}
+	Queue<MyMessage> msgQueue = new Queue<MyMessage>();
+	Action<MyMessage> ExecuteMsg; 
+	bool connected;
+
+	//socketの初期化
+	public MySocketIOClient(string url){
+		guid=System.Guid.NewGuid();
+		_uuid=guid.ToString();
+		connected=false;
+		socket = new SocketIOClient.Client(url);
+		
+		socket.On("connect", (fn) => {
+			Debug.Log ("succeed connection - socket");
+			Dictionary<string, string> args = new Dictionary<string, string>();
+			args.Add("GUID", _uuid);
+			socket.Emit("connected", args);
+		});
+		socket.On("playerID", (_playerID) => {
+			connected=true;
+			playerID = _playerID.Json.GetFirstArgAs<int>();
+			Debug.Log ("playerID:"+playerID);
+		});
+		//イベント名をクラス外部で自由に設定するため、イベント名はメッセージそのものに持たせる
+		//Node Serverとは、全てイベント名”my message”としてやり取りする
+		socket.On("my message", (data) => {
+			string jsonmsg = data.Json.args[0].ToString();
+			//Debug.Log("RECV message:"+jsonmsg);
+			var msg = MiniJSON.Json.Deserialize(jsonmsg) as DICT;
+			
+			int sen = int.Parse(msg["sender"].ToString());
+			string eve = msg["event"].ToString();
+			DICT mes = msg["message"] as DICT;
+
+			//if(sen!=0){//Node.jsに接続直後、ID:0（誰のIDでもない）からのメッセージを受信するバグが発生したため
+				MyMessage message = new MyMessage(sen,eve,mes);
+				msgQueue.Enqueue(message);
+			//}
+		});
+		socket.Error += (sender, e) => {
+				Debug.LogWarning ("socket Error: " + e.Message );//+ ";"+e.Exception.Message);
+		};
+	}
+
+	//UnityのGameObjectを触れるのはメインスレッドのみなので、受信したメッセージはメインスレッドで取り出す
+	public void OnUpdate(){
+		while(this.msgQueue.Count>0){
+			MyMessage mm = msgQueue.Dequeue();
+			if(ExecuteMsg != null) ExecuteMsg(mm);
+		}
+	}
+	 
+	//メッセージを処理するメソッドを登録する
+	//第1引数が処理したいイベント名、第2引数が処理内容
+	public void AddListener(string eventName, Action<MyMessage> act){
+		ExecuteMsg += (s) => {if(s.eventName==eventName) act(s);};
+	}
+
+	public void Open(){
+		socket.Connect();
+	}
+	public void Close(){
+		this.msgQueue.Clear();
+		socket.Close();
+	}
+	public bool isConnected{get{return socket.IsConnected && this.connected;}}
+
+	//メッセージ送信用の拡張関数
+	//第1引数のイベント名で第2引数のメッセージ（辞書形式）を送信する
+	public void Emit(string _event, DICT message){
+		DICT dict = new DICT();
+		dict.Add("sender",playerID);
+		dict.Add("event",_event);
+		dict.Add("message",message);
+		string json = MiniJSON.Json.Serialize(dict);
+		//イベント名をクラス外部で自由に設定するため、イベント名はメッセージそのものに持たせる
+		//Node Serverとは、全てイベント名”my message”としてやり取りする
+		socket.Emit("my message",json);
+	}
+
+
+}
+
+
+//objectを明示的にキャスト出来なかったため
+static class ParseObject{
+	public static int ToInt(this object o){
+		return int.Parse(o.ToString());
+	}
+	public static float ToFloat(this object o){
+		return float.Parse(o.ToString());
+	}
+}
+
+}
+
---
+++ NodeClient.cs
@@ -0,0 +1,90 @@
+﻿//MySocketIOClientの利用例
+
+//以下の2オブジェクトがシーン上に必要
+//First Parson Controller（タグは"Player"）
+//何らかの3Dオブジェクト（タグは"Rival"）
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MySocketIO;
+
+public class NodeClient : MonoBehaviour {
+
+	MySocketIOClient sock;
+	FPSInputController input;
+	Transform player;
+	Transform rival;
+
+	// Use this for initialization
+	void Awake () {
+		input = GameObject.FindWithTag("Player").GetComponent<FPSInputController>();
+		player = GameObject.FindWithTag("Player").transform;
+		rival = GameObject.FindWithTag("Rival").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(sock!=null && sock.isConnected){
+		Move();
+		SyncPos();
+		sock.OnUpdate();
+		}
+	}
+
+	void Move(){
+		Vector3 vec = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		Dictionary<string, object> dic = new Dictionary<string,object>();
+		dic.Add("x",vec.x);dic.Add("y",vec.y);dic.Add("z",vec.z);
+		sock.Emit("Move",dic);
+	}
+	void SyncPos(){
+		Dictionary<string, object> dic = new Dictionary<string,object>();
+		Vector3 playpos = player.position;
+		Vector3 playrot = player.rotation.eulerAngles;
+		dic.Add("posx",playpos.x);dic.Add("posy",playpos.y);dic.Add("posz",playpos.z);
+		dic.Add("rotx",playrot.x);dic.Add("roty",playrot.y);dic.Add("rotz",playrot.z);
+		sock.Emit("SyncPos",dic);
+	}
+
+	void OnMove(MySocketIOClient.MyMessage msg){
+		if(msg.isMine){
+			Dictionary<string,object> dict = msg.message;
+			Vector3 vec = new Vector3(dict["x"].ToFloat(),dict["y"].ToFloat(),dict["z"].ToFloat());
+			input.directionVector = vec;
+		}
+	}
+	void OnSyncPos(MySocketIOClient.MyMessage msg){
+		if(!msg.isMine){
+			Dictionary<string,object> dict = msg.message;
+			Vector3 pos = new Vector3(dict["posx"].ToFloat(),dict["posy"].ToFloat(),dict["posz"].ToFloat());
+			Vector3 rot = new Vector3(dict["rotx"].ToFloat(),dict["roty"].ToFloat(),dict["rotz"].ToFloat());
+			rival.position = pos;
+			rival.rotation = Quaternion.Euler(rot);
+		}
+	}
+
+	private string text = "http://127.0.0.1:8888/";
+	void OnGUI () {
+		text = GUILayout.TextField(text);
+		if (sock==null || !sock.isConnected) {
+			if (GUILayout.Button ("start Connection")) {
+				Debug.Log ("Try Connection");
+				if(sock==null){
+					sock = new MySocketIOClient(text);
+					sock.AddListener("Move",OnMove);
+					sock.AddListener("SyncPos",OnSyncPos);
+				}
+				sock.Open();
+			}
+		} else {
+			if (GUILayout.Button ("Close Connection")) {
+				Debug.Log ("Closing");
+				sock.Close ();
+			}
+		}
+	}
+
+	void OnApplicationQuit(){if(sock!=null) sock.Close();}
+}
